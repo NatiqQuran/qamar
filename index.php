5116c9b012d77160e05a210b17f65deb3a9095c6
erfanekrami <erfanak964@gmail.com>
2022-10-05 14:04:53 +0330

    add shadowclass and darkmode

--- index.php
+++ index.php
@@ -3,8 +3,8 @@
 /**
  * Plugin Name: Ghamar
  * Author: Natiq dev team
- * Description: Add custom classes to wordpress
- * Version: 0.0.1
+ * Description: Add CSS custom classes to wordpress
+ * Version: 0.0.2
  * Requires at least: 6.0
  * License: GNU General Public License v3.0
  * License URI: http://www.gnu.org/licenses/gpl-3.0.html
@@ -12,12 +12,12 @@
 
 define("GH_PLUGIN_SOURCE", plugin_dir_url( __FILE__ ));
 
-function add_style()
+function add_styles()
 {
-    $GH_STYLES = array("css/block.css", "css/test1.css", "css/test2.css");
+    $GH_STYLES = array("css/style.css", "css/shadow.css", "css/darkmode.css");
     
     foreach ($GH_STYLES as $style) {
         wp_enqueue_style("ghamar_" . $style, GH_PLUGIN_SOURCE . $style);
     }
 }
-add_action('wp_enqueue_scripts', 'add_style');
+add_action('wp_enqueue_scripts', 'add_styles');
