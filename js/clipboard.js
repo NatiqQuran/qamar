705b49720c8dac66b38b06bc5d44a2533f9fd85a
webswimmer <webswimmer2007@gmail.com>
2024-06-20 10:06:56 +0330

    New Update

--- js/clipboard.js
+++ js/clipboard.js
@@ -2,14 +2,13 @@
  * @param {MouseEvent} e
  */
 const copy = (e) => {
-    navigator.clipboard.writeText(e.target.innerText);
+  navigator.clipboard.writeText(e.target.innerText);
 
-    e.target.classList.add("qCopied");
+  e.target.classList.add("qCopied");
 
-    setTimeout(() => e.target.classList.remove("qCopied"), 1000);
-}
+  setTimeout(() => e.target.classList.remove("qCopied"), 1000);
+};
 
 document
-    .querySelectorAll(".qClickToCopyText")
-    .forEach(el => el.addEventListener("click", copy))
-
+  .querySelectorAll(".qClickToCopyText")
+  .forEach((el) => el.addEventListener("click", copy));
