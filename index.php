32795d4ee667550c32b66500cbf4263f11ccdd0d
silence0ne <silenceonew@gmail.com>
2024-06-25 11:57:31 +0330

    Update index.php

--- index.php
+++ index.php
@@ -5,19 +5,25 @@
  * Plugin URI: https://qamar.natiq.net
  * Author: Natiq dev team
  * Description: Add CSS custom classes to wordpress
- * Version: 1.0.1
- * Requires at least: 6.0
+ * Version: 1.0.4
+ * Requires at least: 5.0
  * License: MIT License
  * License URI: https://spdx.org/licenses/MIT.html
  */
-define("Q_PLUGIN_SOURCE", plugin_dir_url(__FILE__));
+efine("Q_PLUGIN_SOURCE", plugin_dir_url(__FILE__));
+define("Q_PLUGIN_DIR", plugin_dir_path(__FILE__));
 
 function add_styles()
 {
-    $Q_STYLES = array("css/box.css", "css/style.css", "css/shadow.css", "css/darkmode.css", "css/font.css");
+    $Q_STYLES = array("css/box.css", "css/style.css", "css/shadow.css", "css/font.css");
 
     foreach ($Q_STYLES as $style) {
-        wp_enqueue_style("Qamar_" . $style, Q_PLUGIN_SOURCE . $style);
+        wp_enqueue_style(
+            "Qamar_" . basename($style, ".css"), 
+            Q_PLUGIN_SOURCE . $style, 
+            array(), 
+            filemtime(Q_PLUGIN_DIR . $style)
+        );
     }
 }
 add_action('wp_enqueue_scripts', 'add_styles');
@@ -29,11 +35,11 @@ function add_scripts()
 
     foreach ($Q_SCRIPTS as $script) {
         wp_enqueue_script(
-            "Qamar_" . $script,
+            "Qamar_" . basename($script, ".js"),
             Q_PLUGIN_SOURCE . $script,
             array(),
-            '1.0.0', 
-            array('strategy'  => 'defer')
+            filemtime(Q_PLUGIN_DIR . $script),
+            true
         );
     }
 }
